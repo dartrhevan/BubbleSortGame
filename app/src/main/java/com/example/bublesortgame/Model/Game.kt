859830bc8bfeafd570a76212405b047af9b9ac4b
dartrhevan <vovasatunkin@mail.ru>
2019-11-27 20:49:56 +0500

    speedup first version

--- app/src/main/java/com/example/bublesortgame/Model/Game.kt
+++ app/src/main/java/com/example/bublesortgame/Model/Game.kt
@@ -1,8 +1,12 @@
 package com.example.bublesortgame.Model
 
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.math.abs
+import kotlin.math.round
+import kotlin.random.Random
 
-class Game(var scores: Int = 0,var lives: Int = 5,val bubbles: ArrayList<Bubble> = arrayListOf(/*Bubble(200f, 300f, Colour.YELLOW, 0)*/),val receivers : Map<Int, Receiver> =
+class Game(var scores: Int = 0,var lives: Int = 55,val bubbles: ArrayList<Bubble> = arrayListOf(/*Bubble(200f, 300f, Colour.YELLOW, 0)*/),val receivers : Map<Int, Receiver> =
                mapOf(0 to Receiver(0, Colour.BLUE), 1 to Receiver(1, Colour.RED), 2 to
                    Receiver(2, Colour.GREEN), 3 to Receiver(3, Colour.YELLOW))) {
     val slider: Slider = Slider(this)
@@ -11,10 +15,14 @@ class Game(var scores: Int = 0,var lives: Int = 5,val bubbles: ArrayList<Bubble>
             scores++
         else lives--
         bubbles.remove(bubble)
+         if(!slider.megaBoost && (scores + 1) % speedUpFrequency == 0)
+             speedUp()
         return lives < 0
     }
 
     fun restart() {
+        bubbleDuration = 2000L
+        sliderDuration = 1500L
         bubbles.clear()
         slider.X = 0f
         scores = 0
@@ -25,13 +33,21 @@ class Game(var scores: Int = 0,var lives: Int = 5,val bubbles: ArrayList<Bubble>
         return slider.act(bottom, getReceiver())
     }
 
+    private fun speedUp() {
+        if(bubbleDuration > 1000L) {
+            bubbleDuration = round(bubbleDuration * 0.9).toLong()
+            sliderDuration = round(sliderDuration * 0.9).toLong()
+        } else slider.megaBoost = true
+    }
+
     private fun getReceiver() : Receiver?
     {
         val r: Receiver = receivers.minBy { abs(slider.centerX - it.value.centerX) }!!.value
         return if(abs(slider.centerX - r.centerX) < DELTA) r else null
     }
     companion object{
-        private val DELTA = 5
+        private var speedUpFrequency = 10
+        private const val DELTA = 5
         var bubbleDuration = 2000L
         var sliderDuration = 1500L
         var ReceiverWidth = 0
--- app/src/main/java/com/example/bublesortgame/Model/Slider.kt
+++ app/src/main/java/com/example/bublesortgame/Model/Slider.kt
@@ -8,7 +8,7 @@ data class Slider(val game: Game, var X: Float = 0f) {
     private val r = Random(Date().time)
     fun act(Y: Float, rec : Receiver?) : Bubble? {
         if(rec == null) return null
-        if(r.nextInt() % 2 == 0 && rec.number != lastLine) {
+        if((megaBoost || r.nextBoolean() ) && rec.number != lastLine) {
             lastLine = rec.number
             val element = Bubble(game/*, rec.X */,Y,Colour.values()[r.nextUInt(4u).toInt()],rec.number)
             game.bubbles.add(element)
@@ -16,6 +16,7 @@ data class Slider(val game: Game, var X: Float = 0f) {
         }
         return null
     }
+    var megaBoost = false
     val centerX: Float
     get() = X + Game.SliderWidth / 2
     private var lastLine: Int = -1
