e740363dcda75703633e6715c0693cd09566ff2d
dartrhevan <vovasatunkin@mail.ru>
2020-05-03 18:56:57 +0500

    db divided to tables

--- app/src/main/java/com/example/bublesortgame/Model/Difficulty.kt
+++ app/src/main/java/com/example/bublesortgame/Model/Difficulty.kt
@@ -1,11 +1,11 @@
 package com.example.bublesortgame.Model
 
-data class Difficulty private constructor(var bubbleDuration : Long, var sliderDuration : Long, var delta : Int ) : Cloneable {
+data class Difficulty private constructor(var bubbleDuration : Long, var sliderDuration : Long, var delta : Int, val code : Int ) : Cloneable {
     companion object {
-        fun getEasyDifficulty() = Difficulty(2500L, 2000L, 4 )
+        fun getEasyDifficulty() = Difficulty(2500L, 2000L, 4, 0 )
 
-        fun getStandardDifficulty() = Difficulty(2000L, 1500L, 12)
+        fun getStandardDifficulty() = Difficulty(2000L, 1500L, 12, 1)
 
-        fun getHardDifficulty() = Difficulty(1500L,  1000L, 20)
+        fun getHardDifficulty() = Difficulty(1500L,  1000L, 20, 2)
     }
 }
--- app/src/main/java/com/example/bublesortgame/results/DBResultsProvider.kt
+++ app/src/main/java/com/example/bublesortgame/results/DBResultsProvider.kt
@@ -3,6 +3,8 @@ package com.example.bublesortgame.results
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.database.Cursor
+import com.example.bublesortgame.Model.Difficulty
+import com.example.bublesortgame.Model.Game
 import com.example.bublesortgame.results.Result
 import java.util.*
 import kotlin.collections.ArrayList
@@ -11,17 +13,27 @@ import kotlin.collections.ArrayList
 class DBResultsProvider(ct: Context)
 {
     private val db = ct.openOrCreateDatabase("recs.db", MODE_PRIVATE, null)
-    init {
-        db.execSQL("CREATE TABLE IF NOT EXISTS results (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, describe TEXT, scores INTEGER NOT NULL, time INTEGER)")
+    private val tables = mapOf(Difficulty.getEasyDifficulty().code to "resultsEasy",
+        Difficulty.getStandardDifficulty().code to "resultsStandard", Difficulty.getHardDifficulty().code to "resultsHard")
+    init {/*
+        db.execSQL("CREATE TABLE IF NOT EXISTS resultsEasy (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, describe TEXT, scores INTEGER NOT NULL, time INTEGER)")
+        db.execSQL("CREATE TABLE IF NOT EXISTS resultsStandard (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, describe TEXT, scores INTEGER NOT NULL, time INTEGER)")
+        db.execSQL("CREATE TABLE IF NOT EXISTS resultsHard (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, describe TEXT, scores INTEGER NOT NULL, time INTEGER)")
+*/
+        for ((c, t) in tables)
+            db.execSQL("CREATE TABLE IF NOT EXISTS $t (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, describe TEXT, scores INTEGER NOT NULL, time INTEGER)")
     }
 
     fun addResult(res: Result) =
-        db.execSQL("INSERT INTO results (describe, scores, time) VALUES ('${res.desc}', ${res.scores}, ${res.date.time})")
+        db.execSQL("INSERT INTO $table (describe, scores, time) VALUES ('${res.desc}', ${res.scores}, ${res.date.time})")
+
+    private val table
+    get() = tables[Game.chosenDifficult.code]
 
 
     fun getRecords() : ArrayList<Result> {
         val res = ArrayList<Result>()
-        val query = db.rawQuery("SELECT * FROM results ORDER BY scores DESC",null)
+        val query = db.rawQuery("SELECT * FROM $table ORDER BY scores DESC",null)
         if(query.moveToFirst())
         //for(i in 0..9) {
             do {
